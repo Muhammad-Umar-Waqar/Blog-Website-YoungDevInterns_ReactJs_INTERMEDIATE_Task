2b2599ba30e6113d83486bc9a3d8dbd68db2f7ac
Muhammad Umar Waqar <mohammthegreat@gmail.com>
2024-05-04 01:45:56 +0500

    My Mega Blog Website

---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,75 @@
+import React, {useState, useEffect} from 'react'
+import appwriteService from '../appwrite/config'
+import { Container, PostCard } from '../components'
+
+
+// function AllPosts() {
+  //     const [posts, setPosts] = useState([])
+  //     useEffect(() => {}, [])
+  //     appwriteService.getPosts([]).then((posts) => {
+  //         if (posts) {
+  //             setPosts(posts.documents)
+  //         }
+  //     })
+  //   return (
+  //     <div className='w-full py-8'>
+  //         <Container>
+  //             <div className='flex flex-wrap'>
+  //                 {posts.map((post) => (
+  //                     <div key={post.$id} className='p-2 w-1/4'>
+  //                         <PostCard {...post} />
+  //                     </div>
+  //                 ))}
+  //             </div>
+  //             </Container>
+  //     </div>
+  //   )
+  // }
+//   function AllPosts() {
+//     const [posts, setPosts] = useState([]);
+  
+//     useEffect(() => {
+//       // Fetch posts when the component mounts
+//       const fetchPosts = async () => {
+//         try {
+//           const response = await appwriteService.getPosts([]);
+//           if (response && response.documents) {
+//             setPosts(response.documents);
+//           }
+//         } catch (error) {
+//           console.error('Error fetching posts:', error);
+//         }
+//       };
+  
+//       fetchPosts();
+//     }, []);
+
+
+  
+  function AllPosts() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        appwriteService.getPosts([]).then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+    }, [])
+ 
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <div className=' grid  md:grid-cols-3 grid-cols-1 justify-items-center place-items-center gap-10'>
+                {posts.map((post) => (
+                    <div key={post.$id} className='p-2 w-[300px]'>
+                        <PostCard {...post} className='' />
+                    </div>
+                ))}
+            </div>
+            </Container>
+    </div>
+  )
+                }
+
+export default AllPosts
+
